2f685768ed83df7434463382c35ea0da654e0d5f
ribe3iro <joaopedrorib@hotmail.com>
2023-07-03 15:57:27 -0300

    feat(relatórios): all 6 reports done

--- relatorios.sql
+++ relatorios.sql
@@ -22,7 +22,7 @@ DROP INDEX IF EXISTS IdxBrazilianMediumLargeAirports;
 CREATE INDEX IdxBrazilianMediumLargeAirports
 ON airports 
 USING BTREE(type)
-WHERE isocountry = 'BR'
+WHERE isocountry = 'BR' AND type IN ('medium_airport', 'large_airport');
 ;
 
 CREATE OR REPLACE FUNCTION get_aeroportos_proximos(nome_cidade TEXT)
@@ -91,12 +91,69 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- alternativa
+-- 4
+CREATE OR REPLACE FUNCTION get_contagem_status_da_escuderia(escuderia_id INTEGER)
+RETURNS TABLE (
+    status TEXT,
+    quantidade_resultados BIGINT
+)
+AS $$
+BEGIN
+  RETURN QUERY
+    SELECT s.status, COUNT(r.statusid) AS quantidade_resultados 
+    FROM status s 
+    JOIN results r ON s.statusid = r.statusid AND r.constructorID = escuderia_id
+    GROUP BY s.status 
+    ORDER BY quantidade_resultados DESC
+    ;
+END;
+$$ LANGUAGE plpgsql;
 
--- SELECT DxC.forename || ' ' || DxC.surname AS nome_completo, COUNT(R.driverID) AS quantidade
--- FROM (results R
--- 	JOIN driver D ON R.driverID = D.driverID AND R.constructorID = 1) DxC
--- 	LEFT JOIN results R ON DxC.resultID = R.resultID AND R.position = 1
--- GROUP BY nome_completo
--- ORDER BY quantidade DESC
--- ;
+-- relatórios piloto
+
+-- 5
+DROP INDEX IF EXISTS IdxResultsWinners;
+CREATE INDEX IdxResultsWinners
+ON results 
+USING BTREE(driverID)
+INCLUDE(raceID)
+WHERE position = 1
+;
+
+CREATE OR REPLACE FUNCTION get_all_vitorias_piloto(piloto_id INTEGER)
+RETURNS TABLE (
+    name TEXT,
+    year INTEGER,
+	vitorias BIGINT
+)
+AS $$
+BEGIN
+  RETURN QUERY
+    SELECT RA.name, RA.year, COUNT(*) AS vitorias
+	FROM results RE
+		JOIN races RA ON RA.raceID = RE.raceID
+	WHERE 
+		RE.driverID = piloto_id AND position = 1
+	GROUP BY ROLLUP(RA.year, RA.name)
+	ORDER BY RA.year NULLS FIRST, RA.name NULLS FIRST
+	;
+END;
+$$ LANGUAGE plpgsql;
+
+-- 6
+CREATE OR REPLACE FUNCTION get_contagem_status_do_piloto(piloto_id INTEGER)
+RETURNS TABLE (
+    status TEXT,
+    quantidade_resultados BIGINT
+)
+AS $$
+BEGIN
+  RETURN QUERY
+    SELECT s.status, COUNT(r.statusid) AS quantidade_resultados 
+    FROM status s 
+    JOIN results r ON s.statusid = r.statusid AND r.driverID = piloto_id
+    GROUP BY s.status 
+    ORDER BY quantidade_resultados DESC
+    ;
+END;
+$$ LANGUAGE plpgsql;
